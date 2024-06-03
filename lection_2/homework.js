ce37f665f41ea5a30619c73c3577d07c900582a6
Egor Nikandrov <egr.nikandrov@gmail.com>
2024-06-03 16:55:45 +0300

    egor nikandrov homework2

--- lection_2/homework.js
+++ lection_2/homework.js
@@ -1,7 +1,7 @@
 //Как исправить "одни пятёрки"?
 
 // var result = [];
-// for (var i = 0; i < 5; i++) {
+// for (let i = 0; i < 5; i++) {  // функциональную область видимости var заменил на блочную let
 //     result[i] = function () {
 //         console.log(i);
 //     };
@@ -17,20 +17,18 @@
 // function getGroup() {
 //     let students = [];
 //     let i = 0;
-//     while (i < 10) {
+//     for (let i = 0; i < 10; i++) {  //while на for
 //         students[i] = function() {
 //             console.log(i);
 //         }
-//         i++
 //     }
-//
 //     return students;
 // }
 //
 // let group = getGroup();
 //
 // group[0](); // 10 как исправить на 0
-// group[5](); // 10                  5
+// group[5](); // 10                 5
 
 //////////////////////////////////////////////////
 
@@ -40,7 +38,7 @@
 // console.log(result1); // Вывод: 24
 // const result2 = multiply(2)(3)(4)(5);
 // console.log(result2); // Вывод: 120
-
+//
 // const result1 = multiply(2)(3)(4);
 //
 // // Пример использования:
@@ -50,6 +48,19 @@
 // const result2 = multiply(5)(2)(3);
 // console.log(result2); // Вывод: 30
 
+// const multiply = (arg) => {
+//     let acc = arg
+//     const m = (number) => {
+//         acc *= number
+//         return m
+//     }
+//     m.valueOf = () => acc //приобразовал к числу
+//     return m
+// }
+// const result2 = multiply(5)(2)(3); //30
+// console.log(+result2);    //использовал унарный плюс
+
+
 /////////////////////////
 // Написать функцию getUniqArray(arr), которая на вход принимает массив чисел и
 // возвращает массив уникальных чисел.
@@ -57,3 +68,11 @@
 //     Текст ошибки: "В getUniqArray был передан невалидный параметр. Аргумент arr
 // должен быть массивом чисел".
 
+// const getUniqArray = (arr) => {
+//     if (!Array.isArray(arr) || arr.some(el => typeof el !== 'number')) {
+//         throw new Error('В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел')
+//     } else return Array.from(new Set(arr))
+// }
+//
+// console.log(getUniqArray([1, 2, 3, 4, 5, 3, 4, 5, 6, 1, 8]));
+// console.log(getUniqArray([1, 2, 3, 4, 5, 3, 4, 5, 6, 1, 8, 'b', 'b','fd']));
